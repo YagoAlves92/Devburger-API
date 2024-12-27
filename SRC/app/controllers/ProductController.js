ddbff04c3743503bd996d9d9e7eebc1c4323a09f
YagoAlves92 <yago.chp22@gmail.com>
2024-12-27 12:10:03 -0300

    DevBurguer-API

---
+++ SRC/app/controllers/ProductController.js
@@ -0,0 +1,34 @@
+import * as Yup from 'yup';
+import Product from '../models/Product';
+
+class ProductController{
+    async store(req , res) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            price: Yup.number().required(),
+            category: Yup.string().required(),
+        });
+        try{
+            schema.validateSync(req.body , {abortEarly: false });
+        }catch(err){
+            return res.status(400).json({ error: err.errors});
+        }
+
+        const { filename: path } = req.file
+        const { name , price , category } = req.body
+
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path,
+        })
+        
+
+        
+            
+
+        return res.status(201).json(product);
+    }
+}
+export default new ProductController();
